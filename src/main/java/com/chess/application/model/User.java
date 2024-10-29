fa322a645c958140d66a7c75d8478eac993c9458
Gus Fowle <gusfowle1@gmail.com>
2024-10-29 20:53:52 +0000

    not fucking working

--- src/main/java/com/chess/application/model/User.java
+++ src/main/java/com/chess/application/model/User.java
@@ -5,13 +5,14 @@ import lombok.Data;
 
 import java.util.List;
 import java.util.UUID;
+import jakarta.persistence.GenerationType;
 
 @Entity
 @Data
 @Table
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String username;
     private String password;
